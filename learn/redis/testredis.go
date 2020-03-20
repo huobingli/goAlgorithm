f45c04c2a9371352b95a6e332932ea59d356392e
huobingli <huobingli@outlook.com>
2020-03-20 18:39:18 +0800

    update

--- learn/redis/testredis.go
+++ learn/redis/testredis.go
@@ -1,20 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
-"github.com/garyburd/redigo/redis"
-"fmt"
-)
-
-func main()  {
-    conn,err := redis.Dial("tcp","47.114.171.118:6379")
-    if err != nil {
-        fmt.Println("connect redis error :",err)
-        return
-	}
-    defer conn.Close()
-}
-=======
 	"fmt"
 	// 导入redigo扩展包
 	"github.com/garyburd/redigo/redis" //go get github.com/garyburd/redigo/redis
@@ -32,4 +18,3 @@ func main() {
 		return
 	}
 }
->>>>>>> 60bef4fde156596249aeaacbfba5b863fc1fcd75
