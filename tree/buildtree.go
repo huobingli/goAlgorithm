5f8fbd3b6abeeb8250a3d500390968cf058c2a3a
huobingli <huobingli@outlook.com>
2020-06-01 23:52:53 +0800

    update

---
+++ tree/buildtree.go
@@ -0,0 +1,25 @@
+// 二叉树的建立
+package main
+
+func main() {
+	//创建根节点
+	root := NewNode(nil, nil)
+	var it Initer
+	it = root
+	it.SetData("root node")
+	//创建左子树
+	a := NewNode(nil, nil)
+	a.SetData("left node")
+	al := NewNode(nil, nil) //左叶子节点
+	al.SetData(100)
+	ar := NewNode(nil, nil) //右叶子节点
+	ar.SetData(3.14)
+	a.Left = al
+	a.Right = ar
+	//创建右子树
+	b := NewNode(nil, nil)
+	b.SetData("right node")
+	root.Left = a
+	root.Right = b
+	PrintBT(root)
+}
