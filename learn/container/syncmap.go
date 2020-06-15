3bd75f5aad95d6f2636d65bc12db86f694aa7302
huobingli <huobingli@outlook.com>
2020-06-16 01:34:45 +0800

    update

--- learn/container/syncmap.go
+++ learn/container/syncmap.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-      "fmt"
-      "sync"
+	"fmt"
+	"sync"
 )
 
 /*
@@ -13,18 +13,19 @@ sync.Map 不能使用 map 的方式进行取值和设置等操作，而是使用
 */
 
 func main() {
-    var scene sync.Map
-    // 将键值对保存到sync.Map
-    scene.Store("greece", 97)
-    scene.Store("london", 100)
-    scene.Store("egypt", 200)
-    // 从sync.Map中根据键取值
-    fmt.Println(scene.Load("london"))
-    // 根据键删除对应的键值对
-    scene.Delete("london")
-    // 遍历所有sync.Map中的键值对
-    scene.Range(func(k, v interface{}) bool {
-        fmt.Println("iterate:", k, v)
-        return true
-    })
+	var scene sync.Map
+	// 将键值对保存到sync.Map
+	scene.Store("greece", 97)
+	scene.Store("london", 100)
+	scene.Store("egypt", 200)
+	// 从sync.Map中根据键取值
+	fmt.Println(scene.Load("london"))
+	// 根据键删除对应的键值对
+	scene.Delete("london")
+	scene.Store("tokyo", 2020)
+	// 遍历所有sync.Map中的键值对
+	scene.Range(func(k, v interface{}) bool {
+		fmt.Println("iterate:", k, v)
+		return true
+	})
 }
