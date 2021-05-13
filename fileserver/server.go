653dd8e19d321f833a3f5117ddcf3bf04f1a0ae8
huobingli <huobingli@outlook.com>
2021-05-13 21:28:54 +0800

    update

--- fileserver/server.go
+++ fileserver/server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/shirou/gopsutil/disk"
@@ -20,42 +24,53 @@ func TimeParseYYYYMMDD(in string, sub string) (out time.Time, err error) {
 	return
 }
 
+func getCurDay() (date int){
+	curTime := time.Now()
+	year := curTime.Year()
+	month := int(curTime.Month())
+	day := curTime.Day()
+	return year * 10000 + month * 100 + day
+}
+
 // 文件格式 日期_创建时间
 func cleanfile(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "start clean file ... please wait !!")
 
-	curTime := time.Now()
+	curDay := getCurDay()
 
-	year := curTime.Year()
-	fmt.Print(year)
-	month := curTime.Month()
-	fmt.Print(month)
-	day := curTime.Day()
-	fmt.Print(day)
+	//files = []string{}
 
-	fmt.Fprint("YYY")
+	_dir, err := ioutil.ReadDir(BaseUploadPath)
+	if err != nil {
+		return
+	}
 
-	//fmt.Print(year + month + day)
+	//suffix = strings.ToLower(suffix) //匹配后缀
 
-	// fmt.Print(curTime.Year(), curTi)
-	// files = []string{}
+	for _, _file := range _dir {
+		if _file.IsDir() {
+			dirname := _file.Name()
+			//print(dirname)
+			//print("not dir")
 
-	// _dir, err := ioutil.ReadDir(BaseUploadPath)
-	// if err != nil {
-	// 	return nil, err
-	// }
+			comm := strings.Index(dirname, "_")
 
-	// suffix = strings.ToLower(suffix) //匹配后缀
+			strDirDate := dirname[:comm]
 
-	// for _, _file := range _dir {
-	// 	if _file.IsDir() {
-	// 		continue //忽略目录
-	// 	}
-	// 	if len(suffix) == 0 || strings.HasSuffix(strings.ToLower(_file.Name()), suffix) {
-	// 		//文件后缀匹配
-	// 		files = append(files, path.Join(dir, _file.Name()))
-	// 	}
-	// }
+			dirDate, err := strconv.Atoi(strDirDate)
+			if err != nil {
+				fmt.Print("strconv.Atoi, err:%v\n", err)
+			}
+
+			if dirDate < curDay - 100 {
+				fmt.Println(dirDate)
+				removeDir := BaseUploadPath + dirname
+				os.RemoveAll(removeDir)
+			}
+
+			continue //忽略目录
+		}
+	}
 
 }
 
