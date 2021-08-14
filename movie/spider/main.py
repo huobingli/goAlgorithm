958f4b1eabd1d95120964226c92b84395eb2ecff
huobingli <huobingli@outlook.com>
2021-08-14 16:14:16 +0800

    update

--- movie/spider/main.py
+++ movie/spider/main.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = "https://en.wikipedia.org/wiki/List_of_current_members_of_the_United_States_Senate"
+link = "http://www.xxi5.cn/list/1-0-0-0-0-0.html"
+base_http_header = "http://www.xxi5.cn"
 
 html = requests.get(link).text
 
 # If you do not want to use requests then you can use the following code below
 # with urllib (the snippet above). It should not cause any issue."""
 soup = BeautifulSoup(html, "lxml")
-res = soup.findAll("span", {"class": "fn"})
+res = soup.findAll("td", {"class": "dt prel nobr"})
 for r in res:
-    print("Name: " + r.find('a').text)
-    table_body=soup.find('senators')
-    rows = table_body.find_all('tr')
-    for row in rows:
-        cols=row.find_all('td')
-        cols=[x.text.strip() for x in cols]
-        print(cols)
+    for subr in r.find_all('a'):
+        print("Down: " + subr["href"])
+    # table_body=soup.find('senators')
+    # # rows = table_body.find_all('tr')
+    # for row in rows:
+    #     cols=row.find_all('td')
+    #     cols=[x.text.strip() for x in cols]
+    #     print(cols)
+# 加入redis
