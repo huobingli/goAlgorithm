93f0ca80b7d3ed6c02d25d1d5c9a62c9b88c07cb
huobingli <huobingli@outlook.com>
2021-10-15 17:54:45 +0800

    update

--- learn/typefunc/iszero.go
+++ learn/typefunc/iszero.go
@@ -6,17 +6,34 @@ import (
 
 // 将int定义为MyInt类型
 type MyInt int
+
 // 为MyInt添加IsZero()方法
 func (m MyInt) IsZero() bool {
-    return m == 0
+	return m == 0
 }
+
 // 为MyInt添加Add()方法
 func (m MyInt) Add(other int) int {
-    return other + int(m)
+	return other + int(m)
 }
+
 func main() {
-    var b MyInt
-    fmt.Println(b.IsZero())
-    b = 1
-    fmt.Println(b.Add(2))
+	var b MyInt
+	fmt.Println(b.IsZero())
+	b = 1
+	fmt.Println(b.Add(2))
+
+	fmt.Println(int(^uint(0) >> 1))
+
+	// x := 1234
+	// s := strconv.Itoa(x)
+
+	aa := "123321"
+	bb := "123321"
+	fmt.Print(aa[0] == bb[0])
+	// if s == s[::-1]{
+	//     print(1)
+	// } else {
+	//     print(0)
+	// }
 }
